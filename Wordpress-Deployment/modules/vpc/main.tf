a3aac4c0c574be2664fbfc76fb4825b84799ef66
aftabn10 <aftab@mediadezines.co.uk>
2026-01-02 16:51:19 +0000

    New module added

--- Wordpress-Deployment/modules/vpc/main.tf
+++ Wordpress-Deployment/modules/vpc/main.tf
@@ -89,4 +89,4 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.aws_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
-}
+}l;l;l
